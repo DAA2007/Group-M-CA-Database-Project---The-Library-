1fd94b40722bf5cfd74fec8348f1af9daa02cf50
NCIBenMcEvoy2005 <x25331971@student.ncirl.ie>
2026-04-01 12:51:36 +0100

    Tables Finished
    Tables had a few name changes and some variables were altered.
    Primary and Foreign keys were put in, they cause an error due to no info being in them.
    New catagories were slotted into file for future tasks

--- SQL/TheLibrary.sql
+++ SQL/TheLibrary.sql
@@ -20,34 +20,42 @@ CREATE TABLE Books(
     Author VARCHAR(100),
     Publisher VARCHAR(35),
     DateOfPublishing int(6),
-    IsBorrowed boolean
+    IsBorrowed boolean,
+    foreign key (BookID) references Borrows(BookID)
 );
 
-CREATE TABLE BorrowedBooks(
+CREATE TABLE Borrows(
 	BookID int(7) primary key,
     Title VARCHAR(50),
     DateBorrowed int(6),
+    BorrowerID int(5),
     ReturnDate int(6),
-    IsOverdue boolean
+    IsOverdue boolean,
+    foreign key (BookID) references Books(BookID),
+    foreign key (Title) references Books(Title)
 );
 
-CREATE TABLE LibraryAccounts(
-	UserID int(5),
-	Fname VARCHAR(15),
-    Lname VARCHAR(15),
-	DateFormed int(6),
-	borrowedBookID int(7)
-);
-
-#Denzel: I am thinking that LibraryAccounts should be part of AccountHolders like sub-tables but I don't know, you guys decide. - 30/03/2026
-#Denzel: Oh ya, should we add a Table called ReturnedBooks since we have BorrowedBooks? - 30/03/2026
 CREATE TABLE AccountHolders(
-	UserID int(5),
+	UserID int(5) primary key,
 	Fname VARCHAR(15),
     Lname VARCHAR(15),
-	SignUpDate int(6)
+	SignUpDate int(6),
+    Borrows int(5),
+    foreign key (UserID) references Borrows(BorrowerID)
 );
 
+# 3 - adding the values to the books
 #INSERT INTO Books VALUES (
 #"1234567","DummyTitle","Comedy","Dummy","Dummy Productions","2000-01-01",Yes
 #);
+
+# 4 - Queries (12 ones required)
+# Denzel's Queries
+# Ben's Queries
+# Faheem's Queries
+
+# Messages
+#Denzel: I am thinking that LibraryAccounts should be part of AccountHolders like sub-tables but I don't know, you guys decide. - 30/03/2026
+#Denzel: Oh ya, should we add a Table called ReturnedBooks since we have BorrowedBooks? - 30/03/2026
+#Ben: I figured we were gonna only need a borrowed books table. As returned books would be taken off of the borrowed table 01/04
+#Ben: We need atleast 3 queries each using group by, aggregates and joins each. For the report we just show what tools we used and what errors we encountered. 01/04
